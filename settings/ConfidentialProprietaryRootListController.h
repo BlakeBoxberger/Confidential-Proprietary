711236d51b2d2c95f4b1b5564dca643311174a7d
NeinZedd9 <blake.boxberger@gmail.com>
2018-07-16 13:46:46 -0700

    Add files via upload

---
+++ settings/ConfidentialProprietaryRootListController.h
@@ -0,0 +1,4 @@
+#import <CepheiPrefs/HBRootListController.h>
+
+@interface ConfidentialProprietaryRootListController : HBRootListController
+@end
---
+++ settings/ConfidentialProprietaryRootListController.m
@@ -0,0 +1,24 @@
+#include "ConfidentialProprietaryRootListController.h"
+#import <Cephei/HBRespringController.h>
+#import <Cephei/HBPreferences.h>
+
+@implementation ConfidentialProprietaryRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
++ (NSString *)hb_shareText {
+	return @"I'm using #Confidential&Proprietary by @NeinZedd9 to add text to my iPhone's lockscreen!";
+}
+
+- (void)respring {
+	[[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil]; // Dismisses keyboard
+	[HBRespringController respringAndReturnTo:[NSURL URLWithString:@"prefs:root=ConfidentialProprietary"]];
+}
+
+@end
